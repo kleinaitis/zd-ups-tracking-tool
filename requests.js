d820a086b1621f3f9fa6304f8a23e9ea08a58848
Jeff <jeffkleinaitis@gmail.com>
2024-05-08 14:18:09 -0400

    feat: Add UPS token generation function, and refactor ticket maps into objects

--- requests.js
+++ requests.js
@@ -1,5 +1,26 @@
 require('dotenv').config();
 
+
+async function generateUPSToken() {
+
+    const formData = {
+        grant_type: 'client_credentials'
+    };
+
+    const response = await fetch('https://onlinetools.ups.com/security/v1/oauth/token', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: 'Basic ' + btoa(`${process.env.UPS_CLIENT_ID}:${process.env.UPS_CLIENT_SECRET}`)
+        },
+        body: new URLSearchParams(formData).toString()
+
+    });
+    const data = await response.json();
+    return data["access_token"];
+}
+
+
 async function getTicketSearchResults() {
     const query = encodeURIComponent(process.env.CUSTOM_FIELD)
 
@@ -14,32 +35,38 @@ async function getTicketSearchResults() {
 
     const responseData = await response.json();
     const tickets = responseData.results;
-    const ticketInfo = new Map();
+    const ticketInfo = []
+
     for (const ticket of tickets) {
-        ticketInfo.set(ticket["id"], ticket["custom_fields"][0]["value"]);
+        let ticketObject = {
+            ticketid: ticket["id"],
+            upsTrackingID: ticket["custom_fields"][0]["value"],
+            shipmentStatus: ""
+        }
+        ticketInfo.push(ticketObject)
     }
     return ticketInfo;
+
 }
 
 async function getUPSTrackingStatus() {
-    const trackingNumbers = await getTicketSearchResults();
+    const ticketList = await getTicketSearchResults();
+    const upsAccessToken = await generateUPSToken();
 
-    for (const [ticketID,trackingID] of trackingNumbers) {
-        const response = await fetch(`https://onlinetools.ups.com/api/track/v1/details/${trackingID}`, {
+    for (const ticket of ticketList) {
+        const response = await fetch(`https://onlinetools.ups.com/api/track/v1/details/${ticket.upsTrackingID}`, {
             method: "GET",
             redirect: "follow",
             headers: {
                 transId: '1234',
                 transactionSrc: 'testing',
-                Authorization: `Bearer ${process.env.UPS_ACCESS_TOKEN}`
+                Authorization: `Bearer ${upsAccessToken}`
             },
         });
         const data = await response.json();
-        const ticketStatus = data.trackResponse.shipment[0].package[0].currentStatus.description
-        console.log(ticketID, ticketStatus)
+        ticket.shipmentStatus = data.trackResponse.shipment[0].package[0].currentStatus.description
+        console.log(ticket)
     }
 }
 
 getUPSTrackingStatus();
-
-
