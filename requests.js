3ba994c2d3be959537dbe869a7620b50a9d05e68
Jeff <jeffkleinaitis@gmail.com>
2025-09-18 07:56:35 -0400

    feat: Add functionality for multiple tracking numbers per ticket

--- requests.js
+++ requests.js
@@ -33,83 +33,90 @@ async function getTicketSearchResults() {
         },
     });
 
-    const responseData = await response.json();
-    const tickets = responseData.results;
-
-    const ticketInfo = []
+    const data = await response.json();
+    const tickets = data.results;
+    const parsedTickets = []
     const zendeskCustomFieldID = parseInt(process.env.CUSTOM_FIELD_ID);
 
     for (const ticket of tickets) {
-        let upsTrackingID = null;
-
+        let shipmentInformation = new Map();
         for (const customField of ticket.custom_fields) {
             if (customField.id === zendeskCustomFieldID) {
-             upsTrackingID = customField.value
-            break
+                // Removes commas and divides string based on spaces and new lines
+                let trackingNumbers = customField.value.replace(/,/g, '').split(/\s|\n/)
+                trackingNumbers.forEach((id) => shipmentInformation.set(id, ''));
+                break
             }
         }
-        let ticketObject = {
-            ticketid: ticket["id"],
-            upsTrackingID: upsTrackingID,
-            shipmentStatus: ""
+        let parsedTicket = {
+            ticketID: ticket["id"],
+            shipmentInformation: shipmentInformation,
         }
-        if (upsTrackingID !== '' && upsTrackingID !== null) {
-            ticketInfo.push(ticketObject)
-        }
+        parsedTickets.push(parsedTicket)
     }
-    return ticketInfo;
+    return parsedTickets;
 }
 
 async function getUPSTrackingStatus() {
     const ticketList = await getTicketSearchResults();
     const upsAccessToken = await generateUPSToken();
 
+
     for (const ticket of ticketList) {
-        try {
-            const response = await fetch(`https://onlinetools.ups.com/api/track/v1/details/${ticket.upsTrackingID}`, {
-                method: "GET",
-                redirect: "follow",
-                headers: {
-                    transId: '1234',
-                    transactionSrc: 'testing',
-                    Authorization: `Bearer ${upsAccessToken}`
-                },
-            });
-            if (response.ok) {
-                const data =  await response.json();
-                ticket.shipmentStatus = data.trackResponse.shipment[0].package[0].currentStatus.description
-            } else {
-                ticketList.pop()
+        for (const trackingID of ticket.shipmentInformation.keys())
+            try {
+                const response = await fetch(`https://onlinetools.ups.com/api/track/v1/details/${trackingID}`, {
+                    method: "GET",
+                    redirect: "follow",
+                    headers: {
+                        transId: '1234',
+                        transactionSrc: 'testing',
+                        Authorization: `Bearer ${upsAccessToken}`
+                    },
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    ticket.shipmentInformation.set(trackingID, data.trackResponse.shipment[0].package[0].currentStatus.description);
+                } else {
+                    ticketList.pop()
+                }
+            } catch (error) {
+                console.error('Error fetching UPS API:', error);
             }
-        } catch (error) {
-            console.error('Error fetching UPS API:', error);
-        }
     }
-        return ticketList;
+    return ticketList;
 }
+
 async function updateZendeskTicket() {
     const ticketList = await getUPSTrackingStatus();
     const results = [];
 
-    const openStatuses = ["On the Way", "Delivered", "Delay"];
-
     for (const ticket of ticketList) {
-        const custom_status = ticket.shipmentStatus !== "Delivered" ? ticket.upsTrackingID : "";
-        const status = openStatuses.includes(ticket.shipmentStatus) ? "open" : "hold";
+
+        let custom_status = [];
+        let ticketBody = [];
+
+        // Map.forEach parameters are v,k rather than k,v
+        ticket.shipmentInformation.forEach((shipmentStatus, trackingNumber) => {
+            if (shipmentStatus !== 'Delivered') {
+                custom_status.push(`\n${trackingNumber}`)
+            }
+            ticketBody.push(`\n${trackingNumber} is "${shipmentStatus}"`)
+        })
 
         const body = JSON.stringify({
             "ticket": {
                 "comment": {
-                    "body": `UPS Tracking Tool:\n ${ticket.upsTrackingID} is "${ticket.shipmentStatus}"`,
+                    "body": `UPS Tracking Tool: ${ticketBody.toString()}`,
                     "public": false
                 },
-                "custom_fields": [{"id": `${process.env.CUSTOM_FIELD_ID}`, "value":`${custom_status}`}],
-                status
+                "custom_fields": [{"id": `${process.env.CUSTOM_FIELD_ID}`, "value":`${custom_status.toString()}`}],
+                "status": "open"
             }
         })
 
         try {
-            const response = await fetch(`https://${process.env.ZENDESK_SUBDOMAIN}.zendesk.com/api/v2/tickets/${ticket.ticketid}`, {
+            const response = await fetch(`https://${process.env.ZENDESK_SUBDOMAIN}.zendesk.com/api/v2/tickets/${ticket.ticketID}`, {
                 method: "PUT",
                 redirect: "follow",
 
@@ -122,16 +129,16 @@ async function updateZendeskTicket() {
             });
 
             if (!response.ok) {
-                console.error(`Failed to update ticket ${ticket.ticketid}. Status: ${response.status}`);
+                console.error(`Failed to update ticket ${ticket.ticketID}. Status: ${response.status}`);
             } else {
                 const data = await response.json();
-                results.push(data); // Collect the results
+                results.push(data); // Collect the results for AWS console
             }
         } catch (error) {
-            console.error(`Error updating ticket ${ticket.ticketid}:`, error.message);
+            console.error(`Error updating ticket ${ticket.ticketID}:`, error.message);
         }
     }
-
     return results;
 }
+
 module.exports = { updateZendeskTicket };
